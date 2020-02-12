523ddadff4ac21c0ec17450456a2e277439aeb70
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-12 10:52:49 -0500

    [+] Component > Hero / Routes > Hero: routerLink using button element & method (send parameter)

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Hero } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +11,21 @@ export class HeroesComponent implements OnInit {
 
     heroes : Hero[] = [];
 
-    constructor( private _heroesService: HeroesService ) {
+    constructor(
+        private _heroesService: HeroesService,
+        private _router: Router
+    ) {
         console .log( 'constructor' );
     }
 
     ngOnInit() {
         this .heroes = this ._heroesService .getAll();
         console .log( 'ngOnInit', this .heroes );
+    }
+
+    showHero( idx: number ) {
+        console .log( 'i', idx );
+        this ._router .navigate([ '/hero', idx ]);      // Redireccionamos al Componente Hero enviando el ID respectivo usando en Router de Angular
     }
 
 }
