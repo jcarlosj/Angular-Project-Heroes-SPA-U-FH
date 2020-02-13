82bc1f742eb2cfb9620de6582b8f8518df37deff
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-12 19:40:35 -0500

    [+] Component > Search: Boostrap Card Columns

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -10,6 +10,7 @@ import { HeroesService, Hero } from '../../services/heroes.service';
 export class SearchComponent implements OnInit {
 
     heroes : Hero[] = [];
+    term : string = '';
 
     constructor(
         private _activatedRoute: ActivatedRoute,
@@ -22,6 +23,7 @@ export class SearchComponent implements OnInit {
         this ._activatedRoute .params .subscribe( params => {
             console .log( 'Params', params );
 
+            this .term = params[ 'term' ];
             /** Obtenemos el Heroe que coincide con el termino de busqueda, parametrizada con el nombre 'term' al método del Service que obtiene ese dato */
             this .heroes = this ._heroesService .search( params[ 'term' ]);
             console .log( 'Found this Heroes', this .heroes );
